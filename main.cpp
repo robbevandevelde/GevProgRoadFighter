cc22ff607e141acefd97de1287b2c1b04528f61c
thibautvangoethem <thibaut.vangoethem@student.uantwerpen.be>
2018-11-18 22:02:50 +0100

    actually with sfml

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 //
 // Created by thibaut on 17.11.18.
 //
-#include <iostream>
-//#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 
 int main(int argc,char* argv[]){
- std::cout<<"het werkt"<<std::endl;
+    sf::RenderWindow window(sf::VideoMode(1000,1000), "ROAD FIGHTERS",sf::Style::Default);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.display();
+    }
 }
