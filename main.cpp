c3a7f0f3f533ecb0464fb98353fc386f20e94988
thibautvangoethem <thibaut.vangoethem@student.uantwerpen.be>
2018-11-18 14:58:54 +0100

    new attempt at sfml with travis

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 //
 // Created by thibaut on 17.11.18.
 //
+#include <SFML/Graphics.hpp>
 
 int main(int argc,char* argv[]){
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(1000,1000), "ROAD FIGHTERS",sf::Style::Default);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.display();
+    }
 }
