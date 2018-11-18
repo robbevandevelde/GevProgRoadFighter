8f2b61e9303c7863d6b7b795661f6e7c8674de47
thibautvangoethem <thibaut.vangoethem@student.uantwerpen.be>
2018-11-18 14:31:58 +0100

    trying to get travis working

---
+++ cmake-build-debug/CMakeFiles/GevProgRoadFighter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GevProgRoadFighter.dir/main.cpp.o"
+  "GevProgRoadFighter.pdb"
+  "GevProgRoadFighter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GevProgRoadFighter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/GevProgRoadFighter.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GevProgRoadFighter.
+# This may be replaced when dependencies are built.
