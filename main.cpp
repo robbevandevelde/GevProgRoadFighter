5085c647bf855fc7cd8b3c551594a12951032291
thibautvangoethem <thibaut.vangoethem@student.uantwerpen.be>
2018-11-18 21:37:24 +0100

    Testing to see if travis will work withour sfml

--- main.cpp
+++ main.cpp
@@ -1,19 +1,9 @@
 //
 // Created by thibaut on 17.11.18.
 //
-#include <SFML/Graphics.hpp>
+#include <iostream>
+//#include <SFML/Graphics.hpp>
 
 int main(int argc,char* argv[]){
-    sf::RenderWindow window(sf::VideoMode(1000,1000), "ROAD FIGHTERS",sf::Style::Default);
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.display();
-    }
+ std::cout<<"het werkt"<<std::endl;
 }
