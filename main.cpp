25b9e2c7fdce8b3077d8f2048315024e7782d4d3
thibautvangoethem <thibaut.vangoethem@student.uantwerpen.be>
2018-11-19 21:30:26 +0100

    began making the design

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc,char* argv[]){
     sf::RenderWindow window(sf::VideoMode(1000,1000), "ROAD FIGHTERS",sf::Style::Default);
     while (window.isOpen())
     {
-        sf::Event event;
+        sf::Event event{};
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
